9bb6d5bdb645c22dd0315c6437312d4bf2d6d5ee
Napadensky <davidnapadensky@gmail.com>
2021-07-12 19:23:51 -0300

    first commit

---
+++ funciones-flecha.js
@@ -0,0 +1,9 @@
+const addition = (num1,num2) => num1 + num2
+const subtraction = (num1,num2) => num1 - num2
+const multiplication = (num1,num2) => num1 * num2
+const division = (num1,num2) => num1 / num2
+
+console.log(addition(2,5));
+console.log(subtraction(2,5));
+console.log(multiplication(2,5));
+console.log(division(2,5));
